f501267a4716a7cc15b8d6ce2a377a1768a56c6e
Anirban Singha <anirbansingha20@gmail.com>
2024-06-06 21:43:10 +0530

    Fixed Image"About Me"

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -18,7 +18,7 @@ function AboutMe() {
       transition={{duration: 0.5}}
       className='w-full lg:w-1/2 lg:p-8'>
     <div className='flex items-center justify-center'>
-      <img className='rounded-2xl' src={about} alt='about' />
+      <img className='rounded-xl h-56' src={about} alt='about' />
     </div>
 
       </motion.div>
