33fae803c03ca157984faf0ce687510e1709c0cd
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2024-08-06 12:30:10 +0000

    Include links to vercel and render

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,6 +12,7 @@ function Projects() {
     {
       title: "My Digital Diary",
       github: "https://github.com/SinghaAnirban005/My-Digital-Diary",
+      vercel: "https://my-digital-diary.vercel.app",
       image: blog,
       description: "The Digital Diary is a modern, intuitive, and secure web application designed to replace traditional paper diaries with a digital solution. It allows users to record, manage, and revisit their daily thoughts, experiences, and memories seamlessly from any device.",
       technologies: ["TAILWIND CSS", "JS", "REACT JS", "APPWRITE", "REDUX-TOOLKIT"],
@@ -20,6 +21,7 @@ function Projects() {
     {
       title: "Spend-Ex",
       github: "https://github.com/SinghaAnirban005/Spend-ex",
+      vercel: "https://spend-ex-2.onrender.com",
       image: "https://igamemom.com/wp-content/uploads/2014/10/Expense-Tracker-2.0-logo.png",
       description: "Spend-Ex aims to empower users to take control of their finances by providing a user-friendly platform to manage their money effectively. By leveraging the MERN stack, the app delivers a fast and interactive experience that helps users make better financial decisions.",
       technologies: ["JS","TAILWIND CSS","NODE JS", "REACT JS", "REDUX TOOLKIT", "EXPRESS JS", "MONGO DB"],
@@ -28,6 +30,7 @@ function Projects() {
     {
       title: "Good-Eggs UI Clone",
       github: "https://github.com/SinghaAnirban005/GoodEggs-UI-Clone",
+      vercel: "https://grocery-store-ui-clone.vercel.app",
       image: good,
       description: "An user interface clone of the Good Eggs website, designed to replicate the look and feel of the original site. Built with React.js and Tailwind CSS, this clone demonstrates my skills in front-end development and UI/UX design by accurately reproducing a modern e-commerce platform's design elements and functionality",
       technologies: ["JS", "REACT JS", "TAILWIND CSS"]
@@ -36,6 +39,7 @@ function Projects() {
     {
       title: "Secure Password Generator",
       github: "https://github.com/SinghaAnirban005/Password-Generator",
+      vercel: "https://password-generator-sand-ten.vercel.app",
       image: pg,
       description: "With the increasing importance of cybersecurity, this project aims to provide an easy and effective solution for generating secure passwords, ensuring better protection against unauthorized access and cyber threats.",
       technologies: ["HTML", "TAILWIND CSS", "JS", "REACT JS"],
@@ -44,6 +48,7 @@ function Projects() {
     {
       title: "Musix Match",
       github: "https://github.com/SinghaAnirban005/Lyrics_Finder",
+      vercel: "https://lyrics-finder-tau.vercel.app",
       image: musix,
       description: "Musix Match is a dynamic web application designed to help music enthusiasts effortlessly search for and access the lyrics of their favorite english songs",
       technologies: ["HTML", "CSS", "JS", "REACT JS", "REDUX-TOOLKIT"],
@@ -86,11 +91,13 @@ function Projects() {
             initial= {{ opacity: 0, x: 100 }}
             transition={{duration: 1}}
             className='w-full max-w-xl lg:w-3/4 -mt-1 mb-6'> 
+              <a href={project.vercel} target='_blank'>
               <h6 className='mb-2 font-semibold text-justify'>{project.title}</h6>
+              </a>
               <p className='mb-4 text-neutral-400 text-justify'>{project.description}</p>
 
               {project.technologies.map((tech, index) => (
-                <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800 cursor-pointer' >
+                <span key={index} className='mr-2  rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800 cursor-pointer' >
                   {tech}
                 </span>
               ))}
